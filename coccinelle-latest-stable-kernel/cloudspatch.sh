5285fbaa97e23a622ef437293ce74cc817b74992
Peter Senna Tschudin <peter.senna@gmail.com>
2015-09-29 22:00:03 +0200

    allow passing arguments to spatch

--- coccinelle-latest-stable-kernel/cloudspatch.sh
+++ coccinelle-latest-stable-kernel/cloudspatch.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # Peter Senna Tschudin <peter.senna@gmail.com>
+# The first argument should be an filename found at:
+#    https://github.com/petersenna/rc.git
+# All other arguments, but the first are passed to spatch
 
 COCCI_DIR=/cocci/rc
 SRC_DIR=/linuxes/$(ls /linuxes)
@@ -15,7 +18,9 @@ cd $SRC_DIR
 # If user specify a .cocci file as argument use it, otherwise use latest.cocci
 echo $1|grep \.cocci$ &> /dev/null
 if [[ $? -eq 0 ]];then
-	spatch -j $CORE_CNT $COCCI_DIR/$1 -dir .
+	# ${@:2} passes all but the first argument
+	spatch -j $CORE_CNT $COCCI_DIR/$1 -dir . ${@:2}
 else
-	spatch -j $CORE_CNT $COCCI_DIR/latest.cocci -dir .
+	# ${@:2} passes all but the first argument
+	spatch -j $CORE_CNT $COCCI_DIR/latest.cocci -dir . ${@:2}
 fi
