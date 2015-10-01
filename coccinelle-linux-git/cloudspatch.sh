8045e043b2a1ecce407cebf74cae826828f7cc6e
Peter Senna Tschudin <peter.senna@gmail.com>
2015-10-01 14:34:11 +0200

    another minor change

--- coccinelle-linux-git/cloudspatch.sh
+++ coccinelle-linux-git/cloudspatch.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 # Peter Senna Tschudin <peter.senna@gmail.com>
 
-if [ "$#" -lt 2 ];then
-	clear
-	echo First argument: URL to the .cocci file for curl
-	echo Second argument: string for git checkout from:
-	echo      linux, linux-next, and linux-staging
-	echo The .git/config file is available at:
-	echo      github.com/petersenna/docker/coccinelle-linux-git
-	echo All other arguments will be passed to spatch
-	exit 1
-fi
-
 CORE_CNT=$(nproc)
 GIT_DIR=/linux
 COCCI_DIR=/cocci
 COCCI_FILE=useme.cocci
+
+if [ "$#" -lt 2 ];then
+	clear
+	echo First argument: URL to the .cocci file for curl
+	echo Second argument: string for git checkout from:
+	cd $GIT_DIR; git branch -a
+	echo All other arguments will be passed to spatch
+	exit 1
+fi
 
 COCCI_URL=$1
 GIT_TAG=$2
