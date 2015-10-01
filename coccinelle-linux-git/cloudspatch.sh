d240c169757ea89bc75cb1a30359a3f9a77d110f
Peter Senna Tschudin <peter.senna@gmail.com>
2015-10-01 09:37:17 +0200

    minor changes

--- coccinelle-linux-git/cloudspatch.sh
+++ coccinelle-linux-git/cloudspatch.sh
@@ -3,8 +3,11 @@
 
 if [ "$#" -lt 2 ];then
 	clear
-	echo First argument: URL to the .cocci file
-	echo Second argument: string to git checkout from linux, linux-next, and staging
+	echo First argument: URL to the .cocci file for curl
+	echo Second argument: string for git checkout from:
+	echo      linux, linux-next, and linux-staging
+	echo The .git/config file is available at:
+	echo      github.com/petersenna/docker/coccinelle-linux-git
 	echo All other arguments will be passed to spatch
 	exit 1
 fi
@@ -17,24 +20,24 @@ COCCI_FILE=useme.cocci
 COCCI_URL=$1
 GIT_TAG=$2
 
-# Creates $COCCI_DIR
+# $COCCI_DIR and $COCCI_FILE
 mkdir $COCCI_DIR
 cd $COCCI_DIR
 curl $COCCI_URL > $COCCI_FILE
 if [ $? -ne 0 ];then
-	echo I could not download the semantic patch with curl.
-	echo Are you sure you gave me a valid URL?
+	echo Error downloading the semantic patch from:
+	echo $COCCI_URL
 	exit 1
 fi
 
+# GIT
 cd $GIT_DIR
 git remote update
 git checkout $GIT_TAG
 if [ $? -ne 0 ];then
-	echo Git did not like your string for checking out...
+	echo Git error for checking out: $GIT_TAG
 	exit 1
 fi
 
 cd $GIT_DIR
 spatch -j $CORE_CNT $COCCI_DIR/$COCCI_FILE -dir . ${@:3}
-
