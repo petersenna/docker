2b2a55e8c56839194d8d8792e476f873ebe02e29
Peter Senna Tschudin <peter.senna@gmail.com>
2015-10-02 16:10:09 +0200

    .cocci from url instead from fixed git repository

--- coccinelle-latest-stable-kernel/cloudspatch.sh
+++ coccinelle-latest-stable-kernel/cloudspatch.sh
@@ -4,23 +4,28 @@
 #    https://github.com/petersenna/rc.git
 # All other arguments, but the first are passed to spatch
 
-COCCI_DIR=/cocci/rc
-SRC_DIR=/linuxes/$(ls /linuxes)
 CORE_CNT=$(nproc)
+COCCI_DIR=/cocci
+COCCI_FILE=useme.cocci
+SRC_DIR=/linuxes/$(ls /linuxes)
 
-# Creates $COCCI_DIR
-mkdir /cocci
-cd /cocci
-git clone https://github.com/petersenna/rc.git
+if [ "$#" -lt 1 ];then
+	echo First argument: URL to the .cocci file for curl
+	echo All other arguments will be passed to spatch
+	exit 1
+fi
+COCCI_URL=$1
+
+# $COCCI_DIR and $COCCI_FILE
+mkdir $COCCI_DIR
+cd $COCCI_DIR
+curl $COCCI_URL > $COCCI_FILE
+if [ $? -ne 0 ];then
+	echo Error downloading the semantic patch from:
+	echo $COCCI_URL
+	exit 1
+fi
 
 cd $SRC_DIR
-
-# If user specify a .cocci file as argument use it, otherwise use latest.cocci
-echo $1|grep \.cocci$ &> /dev/null
-if [[ $? -eq 0 ]];then
-	# ${@:2} passes all but the first argument
-	spatch -j $CORE_CNT $COCCI_DIR/$1 -dir . ${@:2}
-else
-	# ${@:2} passes all but the first argument
-	spatch -j $CORE_CNT $COCCI_DIR/latest.cocci -dir . ${@:2}
-fi
+# ${@:2} mean all but the first
+spatch -j $CORE_CNT $COCCI_DIR/$COCCI_FILE -dir . ${@:2}
